92fca1fa7de8b71468234c8cd92c7098cd0e4da7
olayinka <olayinkaoadedeji@gmail.com>
2023-04-03 05:51:47 -0700

    mycountry

--- src/component/Header.js
+++ src/component/Header.js
@@ -19,3 +19,39 @@ const Header = () => {
 
 }
 export default Header ;
+
+// List five significant features of React
+
+// Speed
+// React basically allows developers to utilize individual parts of their application on both the client-side and the server-side, which ultimately boosts the speed of the development process.
+
+// Flexibility
+// Compared to other frontend frameworks, the React code is easier to maintain and is flexible due to its modular structure. This flexibility, in turn, saves a huge amount of time and cost to businesses.
+
+// Usability
+// Deploying React is fairly easy to accomplish if you have some basic knowledge of JavaScript.
+
+// In fact, an expert JavaScript developer can easily learn all ins and outs of the React framework in a matter of a day or two.
+
+// It helps to build rich user interfaces
+// The quality of the user interface in an application plays an important role. If the user interface is poorly designed, then it lowers the chances of an application to succeed.
+
+// But, if an application has a high-quality UI, then there are better chances that your users will love to use the app.
+
+// It uplifts developers’ productivity
+// Frequent updates often turn into headaches when an app has a complex logic and when a single modification in one component can dramatically affect other components.
+// But, to combat this problem, Facebook has amplified React with the component reusability feature.
+
+
+
+// what are the major advantages of React
+
+// Faster rendering.
+// Stable Code Structure.
+// Handy developer tools.
+// Mobile app development.
+// Easy to Learn, Easy to Use.
+
+// 4. What is the name of the Software Engineer that created React? Also, which company owns React?
+
+// React was created by Jordan Walke, a software engineer at Facebook.
